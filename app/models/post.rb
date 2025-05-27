e27cf17e5db2a6a205eccc440977256ea9aa4098
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-27 20:57:25 +0300

    adding relations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
+  has_many :editors_posts
+  has_many :editors, through: :editors_posts, source: :user
 end
