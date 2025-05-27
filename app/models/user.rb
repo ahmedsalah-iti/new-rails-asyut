b779eab0067a382255318afcb0b097b8c10d9ba6
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-27 05:41:40 +0300

    adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     validates :name, :DOB, :email, :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "not valid email" }
 end
