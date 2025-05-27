d073a2584e000b239eb8d2f209d323446ad98aaf
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-27 20:55:03 +0300

    adding CreateEditorsPostsJoinTable and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_27_175058) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_175428) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -18,6 +18,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_27_175058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
