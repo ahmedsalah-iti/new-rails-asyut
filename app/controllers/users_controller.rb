e2c6bbf3695c687a69435d380380e92f13b99860
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-27 05:30:39 +0300

    adding UsersController Actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,52 @@
 class UsersController < ApplicationController
   def index
-    @users = "hello world"
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      DOB: params[:DOB],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+
+    if @user.save
+      redirect_to @user, notice: "User is successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(
+      name: params[:name],
+      DOB: params[:DOB],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+      redirect_to @user, notice: "User is successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User is successfully deleted."
   end
 end
